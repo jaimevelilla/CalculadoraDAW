191670e0e857f21eec086ee82b3d9f08602a5963
jimenez9397 <delasfaltoalcielo@gmail.com>
2017-11-22 18:12:52 +0100

    división

--- src/calculadoraDAW/calculadoraDAW.java
+++ src/calculadoraDAW/calculadoraDAW.java
@@ -1,10 +1,22 @@
 package calculadoraDAW;
 
+import java.util.Scanner;
+
 public class calculadoraDAW {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		int num1, num2, division;
 
+		System.out.println("primer numero");
+		num1 = teclado.nextInt;
+		System.out.println("segundo numero");
+		num2 = teclado.nextInt;
+		
+		division = num1 / num2;
+		
+		System.out.println("el resultado de la división es "+ division);
 	}
 
 }
